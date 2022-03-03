6eee06515d7c961e123403cc0e92e74d0a320ca5
Virtus-web <nicolas.pedenau@hotmail.fr>
2022-03-03 14:26:30 +0100

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import Axios from 'axios'
+
 
 function Information() {
 
-    const [ image, setImage ] = useState("")
-
-    const addImage = () => {
-        Axios.post("https://mariage-carineetpierre.herokuapp.com/testlist", {
-            image: image
-        })
-    }
-
     return (
 
-        <div>
-            <input type="file" name="test" onChange={(e) => {setImage(e.target.value)}}></input>
-            <button style={{fontSize: '1.3em', margin: '0 0 2em 0', height: '3em', boxShadow: '0 0.5rem 1rem 0.01rem rgba(255, 255, 255, 0.3)'}} onClick={addImage}>Envoyer la réponse</button>
-        </div>
+        <form action="/items" method="post" encType="multipart/form-data">
+            <input type="file" name="test"></input>
+            <input type="submit"></input>
+        </form>
         
     )
 }
